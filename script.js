5a8a31ccb78aea9a94c3ad3a4e41271bb5aea26e
Bruna Flôr <brn.flor@gmail.com>
2022-04-21 20:27:45 +0100

    first set

---
+++ script.js
@@ -0,0 +1,9 @@
+const trueCol1Line1 = "a";
+const cell = document.getElementById("letterA");
+
+cell.addEventListener();
+if (cell.value === "a") {
+  console.log("string");
+} else {
+  console.log("não é string");
+}
