7e5340d105f9506e41d852787c4c4bcd15f448cb
Bruna Flôr <brn.flor@gmail.com>
2022-06-10 16:47:01 +0100

    input validation

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const trueCol1Line1 = "a";
-const cell = document.getElementById("letterA");
+// const trueCol1Line1 = "a";
+// const cell = document.getElementById("letterA");
 
-// cell.addEventListener();
-// if (cell.value === "a") {
-//   console.log("string");
-// } else {
-//   console.log("não é string");
-// }
+// cell.addEventListener("submit", function () {
+//   if (cell.value === "a") {
+//     console.log("string");
+//   } else {
+//     console.log("não é string");
+//   }
+// });
+
+const trueCol1Line1 = "a";
+const cell = document.getElementById("col1line1");
+const submit = document.getElementById("submit");
+
+const trueValue = () => {
+  if (cell.nodeValue === trueCol1Line1) {
+    console.log("valores iguais");
+  } else {
+    console.log("tente de novo");
+  }
+};
+
+submit.addEventListener("click", trueValue);
