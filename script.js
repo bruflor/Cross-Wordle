2c584f3612657939a08242ea68b1ef419e5e9237
Bruna Flôr <brn.flor@gmail.com>
2022-06-15 21:10:00 +0100

    wip for each

--- script.js
+++ script.js
@@ -15,22 +15,39 @@
 const words = [
   {
     word: "florianópolis",
-    direction: "vert",
-    isVisible: true,
+    // direction: "vert",
+    // isVisible: true,
     initialTop: 0,
     initialLeft: 4,
   },
 ];
-
-const createInput = () => {
+const createInput = (id, value = "") => {
   const container = document.getElementById("crosswordle_container");
 
   const newInput = document.createElement("input");
   newInput.setAttribute("class", "charInput");
   newInput.setAttribute("maxlength", "1");
-  // newInput.setAttribute("id", id);
+  newInput.setAttribute("id", id);
+  newInput.setAttribute("value", value);
+  newInput.style.left = "50%";
+  newInput.style.top = "15%";
 
   container.appendChild(newInput);
 };
 
-createInput();
+// const createWordInputGroup = ({ word, initialTop, initialLeft }) => {
+//   for (let i = 0; i < word.length; i++) {
+//     console.log(i);
+//     createInput(i, word[i], `${initialTop * 4}%`, `${initialLeft * 4}%`);
+//   }
+// };
+
+// words.forEach((word) => {
+//   createWordInputGroup(word);
+// });
+
+words.forEach(({ word }) => {
+  for (let i = 0; i < word.length; i++) {
+    createInput(i, word[i]);
+  }
+});
