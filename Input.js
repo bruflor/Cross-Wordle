392f2f156bda18d925f66820bf0f1a9aeb3ce6b6
Bruna Flôr <brn.flor@gmail.com>
2022-06-16 13:48:06 +0100

    disabled visible word

--- Input.js
+++ Input.js
@@ -24,91 +24,91 @@ const words = [
     direction: "horiz",
     initialTop: 4,
     initialLeft: 0,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "fortaleza",
     direction: "horiz",
     initialTop: 0,
     initialLeft: 4,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "palmas",
     direction: "horiz",
     initialTop: 1,
     initialLeft: 2,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "boa-vista",
     direction: "horiz",
     initialTop: 2,
     initialLeft: 3,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "recife",
     direction: "horiz",
     initialTop: 3,
     initialLeft: 4,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "cuiabá",
     direction: "horiz",
     initialTop: 5,
     initialLeft: 1,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "manaus",
     direction: "horiz",
     initialTop: 6,
     initialLeft: 2,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "vitória",
     direction: "horiz",
     initialTop: 7,
     initialLeft: 1,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "macapa",
     direction: "horiz",
     initialTop: 8,
     initialLeft: 0,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "goiania",
     direction: "horiz",
     initialTop: 9,
     initialLeft: 3,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "belém",
     direction: "horiz",
     initialTop: 10,
     initialLeft: 2,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "curitiba",
     direction: "horiz",
     initialTop: 11,
     initialLeft: 1,
-    isVisible: true,
+    isVisible: false,
   },
   {
     word: "salvador",
     direction: "horiz",
     initialTop: 12,
     initialLeft: 4,
-    isVisible: true,
+    isVisible: false,
   },
 ];
 
@@ -130,6 +130,8 @@ const createInput = (
 
   if (isVisible) {
     newInput.setAttribute("value", value);
+    newInput.setAttribute("disabled", "disabled");
+    newInput.classList.add("hiddenBorder");
   }
 
   setWordDirection(direction, newInput, indexPosition, initialTop, initialLeft); // 4% is the amount to move one tile
